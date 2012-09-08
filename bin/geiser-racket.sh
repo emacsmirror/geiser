a89a9860fc716e18516e50d1af796fdcdb1f072f
Jose Antonio Ortega Ruiz <jao@gnu.org>
2012-09-08 19:24:10 +0200

    Racket: new option to specify network interface for REPL server
    In geiser-racket.sh, there's the new option -n, which uses a new
    hostname argument accepted by geiser/user's start-geiser function.

--- bin/geiser-racket.sh
+++ bin/geiser-racket.sh
@@ -9,11 +9,13 @@ exec racket -i -S "$top/racket" -l errortrace -cu "$0" ${1+"$@"}
 (require (lib "cmdline.rkt"))
 
 (define port (make-parameter 0))
+(define host (make-parameter #f (lambda (h) (and (string? h) h))))
 
 (command-line
  "run-racket.sh" (current-command-line-arguments)
  (once-each
+  (("-n" "--hostname") n "Network hostname, or #f for all interfaces" (host n))
   (("-p" "--port") p "Geiser server port" (port (string->number p)))))
 
 (printf "Geiser server running at port ~a~%"
-        ((dynamic-require 'geiser/server 'start-geiser) (port)))
+        ((dynamic-require 'geiser/server 'start-geiser) (port) (host)))
