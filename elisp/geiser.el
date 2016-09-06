25dacf5ccb98cf3978b99debcf84656cf3f9e6bb
Chunyang Xu <xuchunyang.me@gmail.com>
2016-09-06 19:47:24 +0800

    Add link to project homepage
    C-h P (`display-package') can make use of it.

--- elisp/geiser.el
+++ elisp/geiser.el
@@ -1,6 +1,7 @@
 ;;; geiser.el --- GNU Emacs and Scheme talk to each other
 
 ;; Copyright (C) 2009, 2010, 2011, 2012, 2013, 2015 Jose Antonio Ortega Ruiz
+;; URL: http://www.nongnu.org/geiser/
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the Modified BSD License. You should
