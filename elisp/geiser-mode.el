7be1dd2a46d2d025d779a156236240d3b737862f
Chaos Eternal <chaos@shlug.org>
2017-01-27 10:40:09 +0800

    bug fix: error message get displayed

--- elisp/geiser-mode.el
+++ elisp/geiser-mode.el
@@ -155,10 +155,10 @@ With a prefix, revert the effect of `geiser-mode-eval-last-sexp-to-buffer' "
 				geiser-mode-eval-last-sexp-to-buffer))
 	 (str (geiser-eval--retort-result-str ret (when will-eval-to-buffer ""))))
     (cond  ((not will-eval-to-buffer) str)
-	   ((string= "" str))
 	   (err (insert (format "%sERROR:%s"
                                 geiser-mode-eval-to-buffer-prefix
                                 (geiser-eval--error-str err))))
+	   ((string= "" str))
 	   (t (push-mark)
               (insert (format "%s%s" geiser-mode-eval-to-buffer-prefix str))))))
 
