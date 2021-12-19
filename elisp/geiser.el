ba290b4fa57cf11667bfb60adf617c5c1bd6c26f
jao <jao@gnu.org>
2021-12-19 23:06:35 +0000

    Requiring Emacs 25.1, for transient's sake

--- elisp/geiser.el
+++ elisp/geiser.el
@@ -11,7 +11,7 @@
 ;; Maintainer: Jose Antonio Ortega Ruiz (jao@gnu.org)
 ;; Keywords: languages, scheme, geiser
 ;; Homepage: https://gitlab.com/emacs-geiser/
-;; Package-Requires: ((emacs "24.4") (transient "0.3"))
+;; Package-Requires: ((emacs "25.1") (transient "0.3"))
 ;; SPDX-License-Identifier: BSD-3-Clause
 ;; Version: 0.20
 
