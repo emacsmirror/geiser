bc0dde310492de733f9ec528d7aeddd57093d05e
jao <jao@gnu.org>
2022-03-23 23:48:42 +0000

    Missing version tag, and version bumped

--- elisp/geiser.el
+++ elisp/geiser.el
@@ -13,7 +13,7 @@
 ;; Homepage: https://gitlab.com/emacs-geiser/
 ;; Package-Requires: ((emacs "25.1") (transient "0.3") (project "0.8.1"))
 ;; SPDX-License-Identifier: BSD-3-Clause
-;; Version: 0.23
+;; Version: 0.23.2
 
 ;;; Commentary:
 
