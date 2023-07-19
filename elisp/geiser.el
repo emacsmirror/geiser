761c65be5763a0cac161f2d9ea9dec510f58b83d
jao <jao@gnu.org>
2023-07-19 23:51:28 +0100

    version bump

--- elisp/geiser.el
+++ elisp/geiser.el
@@ -1,6 +1,6 @@
 ;;; geiser.el --- GNU Emacs and Scheme talk to each other -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2009-2013, 2015, 2018, 2021-2022 Jose Antonio Ortega Ruiz
+;; Copyright (C) 2009-2013, 2015, 2018, 2021-2023 Jose Antonio Ortega Ruiz
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the Modified BSD License. You should
@@ -13,7 +13,7 @@
 ;; Homepage: https://gitlab.com/emacs-geiser/
 ;; Package-Requires: ((emacs "25.1") (project "0.8.1"))
 ;; SPDX-License-Identifier: BSD-3-Clause
-;; Version: 0.28.2
+;; Version: 0.29
 
 ;;; Commentary:
 
